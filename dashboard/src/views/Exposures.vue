b1c36f44f6cff0e91206beee6f9144ca6fb38662
Kenmusembi <kenmusembi21@gmail.com>
2020-03-13 09:02:11 +0300

    updated exposures

--- dashboard/src/views/Exposures.vue
+++ dashboard/src/views/Exposures.vue
@@ -1,300 +1,84 @@
 <template>
-  <v-container
-    fill-height
-    fluid
-    grid-list-xl
-  >
-    <v-layout wrap>
-
-      <!-- Start Cards -->
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
-        <material-stats-card
-          color="green"
-          icon="mdi-store"
-          title="Revenue"
-          value="$34,245"
-          sub-icon="mdi-calendar"
-          sub-text="Last 24 Hours"
-        />
-      </v-flex>
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
-        <material-stats-card
-          color="orange"
-          icon="mdi-content-copy"
-          title="Used Space"
-          value="49/50"
-          small-value="GB"
-          sub-icon="mdi-alert"
-          sub-icon-color="error"
-          sub-text="Get More Space..."
-          sub-text-color="text-primary"
-        />
-      </v-flex>
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
-        <material-stats-card
-          color="red"
-          icon="mdi-information-outline"
-          title="Fixed Issues"
-          value="75"
-          sub-icon="mdi-tag"
-          sub-text="Tracked from Github"
-        />
-      </v-flex>
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
-        <material-stats-card
-          color="info"
-          icon="mdi-twitter"
-          title="Followers"
-          value="+245"
-          sub-icon="mdi-update"
-          sub-text="Just Updated"
-        />
-      </v-flex>
-
-      <!-- End Cards -->
-
-      <!-- Start Graphs -->
-      <v-flex
-        md12
-        sm12
-        lg6
-      >
-        <material-chart-card
-            :data="dailySalesChart.data"
-            :options="dailySalesChart.options"
-            color="info"
-            type="Line"
-          >
-            <h4 class="title font-weight-light">Daily Sales</h4>
-            <p class="category d-inline-flex font-weight-light">
-              <v-icon
-                color="green"
-                small
-              >
-                mdi-arrow-up
-              </v-icon>
-              <span class="green--text">55%</span>&nbsp;
-              increase in today's sales
-            </p>
-
-            <template slot="actions">
-              <v-icon
-                class="mr-2"
-                small
-              >
-                mdi-clock-outline
-              </v-icon>
-              <span class="caption grey--text font-weight-light">updated 4 minutes ago</span>
-            </template>
-        </material-chart-card>
-
-    </v-flex>
-
-      <v-flex
-        md12
-        sm12
-        lg6
-      >
-        <material-chart-card
-            :data="emailsSubscriptionChart.data"
-            :options="emailsSubscriptionChart.options"
-            :responsive-options="emailsSubscriptionChart.responsiveOptions"
-            color="red"
-            type="Bar"
-          >
-            <h4 class="title font-weight-light">Email Subscription</h4>
-            <p class="category d-inline-flex font-weight-light">Last Campaign Performance</p>
-
-            <template slot="actions">
-              <v-icon
-                class="mr-2"
-                small
-              >
-                mdi-clock-outline
-              </v-icon>
-              <span class="caption grey--text font-weight-light">updated 10 minutes ago</span>
-            </template>
-        </material-chart-card>
-      </v-flex>
-          
-
-      <!-- End Graphs -->
-                    
-      
-    </v-layout>
-  </v-container>
+  <section class="charts">
+        <h3>Exposure Rates</h3>
+        <vue-highcharts :options="options" ref="barChart"></vue-highcharts>
+    </section>
 </template>
 
-<script>
-export default {
-  data () {
-    return {
-      dailySalesChart: {
-        data: {
-          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          series: [
-            [12, 17, 7, 17, 23, 18, 38]
-          ]
-        },
-        options: {
-          lineSmooth: this.$chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      dataCompletedTasksChart: {
-        data: {
-          labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-          series: [
-            [230, 750, 450, 300, 280, 240, 200, 190]
-          ]
-        },
-        options: {
-          lineSmooth: this.$chartist.Interpolation.cardinal({
-            tension: 0
-          }),
-          low: 0,
-          high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
-          chartPadding: {
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0
-          }
-        }
-      },
-      emailsSubscriptionChart: {
-        data: {
-          labels: ['Ja', 'Fe', 'Ma', 'Ap', 'Mai', 'Ju', 'Jul', 'Au', 'Se', 'Oc', 'No', 'De'],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
 
-          ]
-        },
-        options: {
-          axisX: {
-            showGrid: false
-          },
-          low: 0,
-          high: 1000,
-          chartPadding: {
-            top: 0,
-            right: 5,
-            bottom: 0,
-            left: 0
-          }
-        },
-        responsiveOptions: [
-          ['screen and (max-width: 640px)', {
-            seriesBarDistance: 5,
-            axisX: {
-              labelInterpolationFnc: function (value) {
-                return value[0]
-              }
-            }
-          }]
-        ]
-      },
-      headers: [
-        {
-          sortable: false,
-          text: 'ID',
-          value: 'id'
-        },
-        {
-          sortable: false,
-          text: 'Name',
-          value: 'name'
-        },
-        {
-          sortable: false,
-          text: 'Salary',
-          value: 'salary',
-          align: 'right'
-        },
-        {
-          sortable: false,
-          text: 'Country',
-          value: 'country',
-          align: 'right'
-        },
-        {
-          sortable: false,
-          text: 'City',
-          value: 'city',
-          align: 'right'
-        }
-      ],
-      items: [
-        {
-          name: 'Dakota Rice',
-          country: 'Niger',
-          city: 'Oud-Tunrhout',
-          salary: '$35,738'
-        },
-        {
-          name: 'Minerva Hooper',
-          country: 'Curaçao',
-          city: 'Sinaai-Waas',
-          salary: '$23,738'
-        }, {
-          name: 'Sage Rodriguez',
-          country: 'Netherlands',
-          city: 'Overland Park',
-          salary: '$56,142'
-        }, {
-          name: 'Philip Chanley',
-          country: 'Korea, South',
-          city: 'Gloucester',
-          salary: '$38,735'
-        }, {
-          name: 'Doris Greene',
-          country: 'Malawi',
-          city: 'Feldkirchen in Kārnten',
-          salary: '$63,542'
-        }
-      ],
-      tabs: 0,
-      list: {
-        0: false,
-        1: false,
-        2: false
+<script>
+import VueHighcharts from "vue2-highcharts";
+import Highcharts from "highcharts";
+
+const data = {
+  chart: {
+    type: "bar"
+  },
+  title: {
+    text: "Exposures Rates Per Cadre"
+  },
+
+  xAxis: {
+    categories: ["Doctor", "Clinical Officer", "Nurse", "Student", "Laboratory Technologist", "Cleaner", "Waste Handler", "VCT Counsellor", "Other"],
+    title: {
+      text: null
+    }
+  },
+  yAxis: {
+    min: 0,
+    title: {
+      text: "Number",
+      align: "high"
+    },
+    labels: {
+      overflow: "justify"
+    }
+  },
+  tooltip: {
+    valueSuffix: " hundreds"
+  },
+  plotOptions: {
+    bar: {
+      dataLabels: {
+        enabled: true
       }
     }
   },
-  methods: {
-    complete (index) {
-      this.list[index] = !this.list[index]
-    }
-  }
-}
-</script>
+  legend: {
+    layout: "vertical",
+    align: "right",
+    verticalAlign: "top",
+    x: -40,
+    y: 80,
+    floating: true,
+    borderWidth: 1,
+    backgroundColor:
+      (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || "#FFFFFF",
+    shadow: true
+  },
+  credits: {
+    enabled: false
+  },
+  series: [
+    {
+      name: "Reported Exposures",
+      data: [107, 31, 635, 203, 2, 55, 8, 90, 21]
+    },
+    {
+      name: "Registered HCWs",
+      data: [133, 156, 947, 408, 6, 82, 67, 104, 78]
+    },
 
+  ]
+};
+export default {
+  components: {
+    VueHighcharts
+  },
+  data() {
+    return {
+      options: data
+    };
+  }
+};
+</script>
