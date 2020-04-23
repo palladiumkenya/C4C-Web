4dd4a02e030ffb37c841eb7d9ab93d73849c3a75
Kenmusembi <kenmusembi21@gmail.com>
2020-04-23 15:11:25 +0300

    HBV report filters added

--- src/views/HbvReports.vue
+++ src/views/HbvReports.vue
@@ -1,37 +1,239 @@
-
 <template>
-  <v-card>
-    <h3/>
-    <highcharts
-      ref="columnChart"
-      :options="barOptionsHBV"/>
+  <v-container
+    fill-height
+    fluid
+    grid-list-xl
+  >
+    <v-layout wrap>
 
-  </v-card>
+      <!-- Start Cards -->
+<template>
+        <!-- Start filters -->
 
+        <v-layout >
+          <v-flex
+            xs12
+            md6
+            lg3
+          >
+            <template>
+              <v-combobox
+                v-model="counties"
+                :items="all_counties"
+                item-text="name"
+                item-value="id"
+                label="Select County"
+                v-on:change="getSubCounties"
+                multiple
+                clerable
+                persistent-hint
+                chips/>
+            </template>
+          </v-flex>
+          <v-flex
+            xs12
+            md6
+            lg3
+          >
+            <template>
+              <v-combobox
+                v-model="subcounties"
+                :items="all_subcounties"
+                item-text="name"
+                item-value="id"
+                label="Select Sub-County"
+                :disabled="active"
+                v-on:change="facilitySubCounty"
+                multiple
+                clerable
+                persistent-hint
+                chips/>
+
+            </template>
+          </v-flex>
+
+          <v-flex
+            xs12
+            md6
+            lg3
+          >
+
+            <template>
+
+              <v-combobox
+                v-model="facility"
+                :items="fac"
+                item-text="partner"
+                item-value="id"
+                label="Select Partner"
+                multiple
+                clerable
+                disabled
+                persistent-hint
+                chips/>
+
+            </template>
+          </v-flex>
+
+          <v-flex
+            xs12
+            md6
+            lg3
+          >
+
+            <template>
+
+              <v-combobox
+                :items="all_facilities_level"
+                label="Select Facility Level"
+                v-on:change="facilityLevel"
+                :disabled="active_level"
+                multiple
+                clerable
+                persistent-hint
+                chips/>
+
+            </template>
+          </v-flex>
+
+          <v-flex
+            xs12
+            md6
+            lg3
+          >
+            <template>
+              <v-combobox
+                v-model="facility"
+                :items="fac"
+                item-text="name"
+                item-value="id"
+                label="Select Facility"
+                v-on:change="facilityFilter"
+                :disabled="active_fac"
+                multiple
+                clerable
+                persistent-hint
+                chips/>
+
+            </template>
+          </v-flex>
+        </v-layout>
+
+        <template>
+          <v-flex xs12 sm6 md2>
+            <v-menu
+              ref="menu1"
+              :close-on-content-click="false"
+              v-model="menu1"
+              :nudge-right="40"
+              :return-value.sync="startDate"
+              lazy
+              transition="scale-transition"
+              offset-y
+              full-width
+              min-width="290px"
+            >
+              <v-text-field
+                slot="activator"
+                v-model="startDate"
+                label="Start Date"
+                prepend-icon="mdi-calendar"
+                readonly
+              ></v-text-field>
+              <v-date-picker :dark="true" v-model="startDate" no-title scrollable :max="endDate" :min="minDate">
+                <v-spacer></v-spacer>
+                <v-btn flat color="primary" @click="menu1 = false">Cancel</v-btn>
+                <v-btn flat color="primary" @click="click();$refs.menu1.save(startDate);click">OK</v-btn>
+              </v-date-picker>
+            </v-menu>
+          </v-flex>
+          <v-flex xs12 sm6 md2>
+            <v-menu
+              ref="menu"
+              :close-on-content-click="false"
+              v-model="menu"
+              :nudge-right="40"
+              :return-value.sync="endDate"
+              lazy
+              transition="scale-transition"
+              offset-y
+              full-width
+              min-width="290px"
+            >
+              <v-text-field
+                slot="activator"
+                v-model="endDate"
+                label="End Date"
+                prepend-icon="mdi-calendar"
+                readonly
+              ></v-text-field>
+              <v-date-picker :dark="true" v-model="endDate" no-title scrollable :max="maxDate" :min="startDate">
+                <v-spacer></v-spacer>
+                <v-btn flat color="primary" @click="menu = false">Cancel</v-btn>
+                <v-btn flat color="primary" @click="click();$refs.menu.save(endDate)">OK</v-btn>
+              </v-date-picker>
+            </v-menu>
+          </v-flex>
+        </template>
+
+        <template>
+          <v-btn
+            block
+            color="secondary"
+            dark
+            @click="click">Filter</v-btn>
+        </template>
+
+        <!-- End filters -->
+      </template>
+      <template>
+        <v-card>
+          <h3/>
+          <highcharts
+            ref="columnChart"
+            :options="barOptionsHBV"/>
+
+        </v-card>
+
+      </template>
+
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 import { Chart } from 'highcharts-vue'
-import Highcharts from 'highcharts'
-import exportingInit from 'highcharts/modules/exporting'
 import axios from 'axios'
-import { mapGetters } from 'vuex'
+import Highcharts from 'highcharts'
 import moment from 'moment'
+import { mapGetters } from 'vuex'
 
-// SeriesLabel(Highcharts);
-exportingInit(Highcharts)
+import { EventBus } from './../event-bus.js'
 
 export default {
-  computed: {
-    ...mapGetters({
-      user: 'auth/user'
-    })
-  },
   components: {
     highcharts: Chart
   },
   data () {
     return {
+      menu: false,
+      menu1: false,
+      startDate: '2016-01-01',
+      maxDate: new Date().toISOString().substr(0, 10),
+      minDate: '2016-01-01',
+      endDate: new Date().toISOString().substr(0, 10),
+      facility: '',
+      counties: '',
+      subcounties: '',
+      fac: [],
+      all_facilities: [],
+      all_facilities_level: ['Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5 and Above'],
+      all_counties: [],
+      all_subcounties: [],
+      active: true,
+      active_fac: true,
+      active_level: true,
+
       barOptionsHBV: {
         chart: {
           type: 'column',
@@ -79,13 +281,242 @@ export default {
         ]
       },
       seriesname: [1, 2, 3],
-      hbvs: []
+      hbvs: [],
+
+      s: [],
+      userz: [],
+      load: true,
+
+      fac_filt: [],
+      exp_filt: [],
+      us_filt: [],
+      fac_filtl: [],
+      exp_filtl: [],
+      us_filtl: [],
+      fac_filtf: [],
+      exp_filtf: [],
+      us_filtf: []
     }
   },
+  computed: {
+    ...mapGetters({
+      user: 'auth/user'
+    })
+  },
   created () {
+    if (this.user === null) {
+      alert('Not admin')
+      this.$router.replace({
+        name: 'login'
+      })
+    }
     this.getImmunizations()
+    this.getFacilities()
+    this.getCounties()
   },
   methods: {
+    click () {
+      let exp = []
+      var dates = {
+        convert:function(d) {
+          return (
+            d.constructor === Date ? d :
+            d.constructor === Array ? new Date(d[0],d[1],d[2]) :
+            d.constructor === Number ? new Date(d) :
+            d.constructor === String ? new Date(d) :
+            typeof d === "object" ? new Date(d.year,d.month,d.date) :
+            NaN
+          );
+        },
+        inRange:function(d,start,end) {
+          return (
+            isFinite(d=this.convert(d).valueOf()) &&
+            isFinite(start=this.convert(start).valueOf()) &&
+            isFinite(end=this.convert(end).valueOf()) ?
+            start <= d && d <= end :
+            NaN
+          );
+        }
+      }
+      for (var e in this.s) {
+        var i = new Date(this.s[e].created_at).toISOString().substr(0, 10)
+        if (dates.inRange(i,this.startDate,this.endDate)){
+          exp.push(this.s[e])
+        }
+      }
+      this.getMonth(exp)
+    },
+    getFacilities () {
+      axios.get('facilities')
+        .then((facilities) => {
+          console.log(facilities.data)
+          this.all_facilities = facilities.data.data
+        })
+        .catch(error => console.log(error.message))
+    },
+    getCounties () {
+      axios.get('counties')
+        .then((counties) => {
+          console.log(counties.data.data)
+          this.all_counties = counties.data.data
+        })
+        .catch(error => console.log(error.message))
+    },
+
+    getSubCounties (a) {
+      console.log(a)
+      if (a.length > 0) {
+        this.active = false
+        this.all_subcounties = []
+        for (var i in a) {
+          axios.get(`subcounties/${a[i].id}`)
+            .then((subcounties) => {
+              // console.log(subcounties.data)
+              this.all_subcounties = this.all_subcounties.concat(subcounties.data.data)
+            })
+            .catch(error => console.log(error.message))
+        }
+        this.facilityCounty(a)
+      } else {
+        this.active = true
+        this.facilityCounty(a)
+      }
+    },
+    facilityCounty (a) {
+      this.us_filt =[],this.fac_filt = [], this.exp_filt = []
+      if (a.length > 0) {
+        for (var c in a) {
+          // console.log(a[c].name)
+          for (var f in this.all_facilities) {
+            if (this.all_facilities[f].county == a[c].name) {
+              this.fac_filt.push(this.all_facilities[f])
+              for (var u in this.userz) {
+                if (this.userz[u].facility_id == this.all_facilities[f].id) {
+                  this.us_filt.push(this.userz[u])
+                }
+              }
+            }
+          }
+          for (var ex in this.hs) {
+            if (this.s[ex].county == a[c].name) {
+              this.exp_filt.push(this.s[ex])
+              // console.log(this.s[ex])
+            }
+          }
+        }
+     //   this.getTest(this.us_filt)
+        this.getMonth(this.exp_filt)
+        this.fac = this.fac_filt.sort()
+      } else {
+        this.fac = this.all_facilities
+     //   this.getTest(this.userz)
+        this.getMonth(this.s)
+      }
+    },
+    facilitySubCounty (a) {
+      this.exp_filtl = [], this.fac_filtl = [], this.us_filtl = []
+      this.active_level = false
+      if (a.length > 0) {
+        for (var c in a) {
+          // console.log(a[c].name)
+          for (var f in this.fac_filt) {
+            if (this.fac_filt[f].sub_county == a[c].name) {
+              this.fac_filtl.push(this.fac_filt[f])
+            }
+          }
+          for (var ex in this.exp_filt) {
+            if (this.exp_filt[ex].sub_county == a[c].name) {
+              this.exp_filtl.push(this.exp_filt[ex])
+            }
+          }
+          for (var u in this.us_filt) {
+            if (this.us_filt[u].sub_county == a[c].name) {
+              this.us_filtl.push(this.us_filt[u])
+            }
+          }
+        }
+      //  this.getTest(this.us_filtl)
+        this.getMonth(this.exp_filtl)
+        this.fac = this.fac_filtl.sort()
+      } else {
+        this.fac = this.fac_filt
+      //  this.getTest(this.us_filt)
+        this.getMonth(this.exp_filt)
+        this.active_level = true
+      }
+    },
+
+    facilityLevel (a) {
+      this.fac_filtf = [], this.exp_filtf = [], this.us_filtf = []
+      this.active_fac = false
+      console.log(a)
+      if (a.length > 0) {
+        for (var c in a) {
+          for (var f in this.fac_filtl) {
+            if (this.fac_filtl[f].level == a[c]) {
+              this.fac_filtf.push(this.fac_filtl[f])
+            } else if (a[c]== 'Level 5 and Above') {
+              if (Number(this.fac_filtl[f].level.slice(6,7)) >= 5) {
+                this.fac_filtf.push(this.fac_filtl[f])
+              }
+            }
+          }
+          for (var ex in this.exp_filtl) {
+            if (this.exp_filtl[ex].facility_level == a[c]) {
+              this.exp_filtf.push(this.exp_filtl[ex])
+            } else if (a[c]== 'Level 5 and Above') {
+              if (Number(this.exp_filtl[ex].facility_level.slice(6,7)) >= 5) {
+                this.exp_filtf.push(this.exp_filtl[ex])
+              }
+            }
+          }
+          for (var u in this.us_filtl) {
+            if (this.us_filtl[u].facility_level == a[c]) {
+              this.us_filtf.push(this.us_filtl[u])
+            } else if (a[c]== 'Level 5 and Above') {
+              if (Number(this.us_filtl[u].facility_level.slice(6,7)) >= 5) {
+                this.us_filtf.push(this.us_filtl[u])
+              }
+            }
+          }
+
+        }
+    //    this.getTest(this.us_filtf)
+        this.getMonth(this.exp_filtf)
+        this.fac = this.fac_filtf.sort()
+      } else {
+        this.fac = this.fac_filtl
+     //   this.getTest(this.us_filtl)
+        this.active_fac = true
+        this.getMonth(this.exp_filtl)
+      }
+    },
+
+    facilityFilter (a) {
+      let b = [], e = [], us = []
+
+      if (a.length > 0) {
+        for (var c in a) {
+          for (var ex in this.exp_filtf) {
+            if (this.exp_filtf[ex].facility_name == a[c].name) {
+              e.push(this.exp_filtf[ex])
+            }
+          }
+          for (var u in this.us_filtf) {
+            if (this.us_filtf[u].facility_name == a[c].name) {
+              us.push(this.us_filtf[u])
+            }
+          }
+
+        }
+       // this.getTest(us)
+        this.getMonth(e)
+      } else {
+        //this.getTest(this.us_filtf)
+        this.getMonth(this.exp_filtf)
+      }
+    },
+
     getImmunizations () {
       if (this.user.role_id === 1) {
         axios.get('immunizations/all/disease/1')
@@ -160,7 +591,21 @@ export default {
           i = 11
         }
       }
-      this.getHBV()
+   //   this.getHBV()
+      this.getMonth(this.s)
+    },
+    getMonth (list) {
+    /*  this.seriesdata = []
+      for (var vac in this.seriesname) {
+        this.seriesdata.push(this.getNum(vac))
+      }
+      this.barOptionsHBV.series[0].data = this.seriesdata*/
+      // console.log(list)
+      var wdata = []
+      for (var i in this.seriesname) {
+        wdata.push(this.getNum(this.barOptionsHBV.xAxis.categories[i], list))
+      }
+      this.barOptionsHBV.series[0].data = wdata
     }
   }
 }
